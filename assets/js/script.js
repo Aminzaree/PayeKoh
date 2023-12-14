c4f62e124644e3d16aeef926e5c98d38f09c538c
Amin <124307503+Aminzaree@users.noreply.github.com>
2023-12-14 15:27:59 +0330

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
 /*----------------------------------------------------------------*/
+/*-----------------------------Loading----------------------------*/
+/*----------------------------------------------------------------*/
+
+setTimeout(function(){
+    // Hide the loading container when the delay is over
+    const loadingContainer = document.getElementById("loading-container");
+    loadingContainer.style.display = "none";
+}, 2000);
+
+
+
+/*----------------------------------------------------------------*/
 /*---------------------------Mobile Html--------------------------*/
 /*----------------------------------------------------------------*/
 
@@ -49,26 +61,43 @@ window.addEventListener("scroll", function () {
 })
 
 
+
+/*----------------------------------------------------------------*/
+/*----------------------------Progress Bar------------------------*/
+/*----------------------------------------------------------------*/
+
+
+const progressBar = document.querySelector(".progress-bar");
+
+window.addEventListener("scroll", function(){
+    const winScroll = this.window.pageYOffset;
+    const height = this.document.documentElement.scrollHeight - this.window.innerHeight;
+    const scrolled = (winScroll/height) * 100;
+    progressBar.style.width = `${scrolled}%`
+})
+
+
+
 /*----------------------------------------------------------------*/
 /*--------------------------To Top Button-------------------------*/
 /*----------------------------------------------------------------*/
 
-// window.addEventListener("scroll", function () {
-//     const toTop = document.querySelector("#toTopBtn");
-//     if (this.window.pageYOffset > 100) {
-//         toTop.classList.add("active");
-//     } else {
-//         toTop.classList.remove("active");
-//     }
-// })
+window.addEventListener("scroll", function () {
+    const toTop = document.querySelector("#toTopBtn");
+    if (this.window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
 
 
-// function backToTop() {
-//     window.scrollTo({
-//         top: 0,
-//         behavior: "smooth"
-//     });
-// }
+function backToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
 
 /*----------------------------------------------------------------*/
 /*----------------------------Counter Up--------------------------*/
@@ -99,93 +128,3 @@ counters.forEach(counter => {
 
     updateCounter()
 })
-
-
-/*----------------------------------------------------------------*/
-/*----------------------------Message Box-------------------------*/
-/*----------------------------------------------------------------*/
-
-
-function openMessageBox() {
-    let messageBox = document.querySelector("#messageBox");
-    messageBox.classList.add("active");
-}
-
-function closeMessageBox() {
-    let messageBox = document.querySelector("#messageBox");
-    messageBox.classList.remove("active");
-}
-
-
-/*----------------------------------------------------------------*/
-/*----------------------------Progress Bar------------------------*/
-/*----------------------------------------------------------------*/
-
-
-const progressBar = document.querySelector(".progress-bar");
-
-window.addEventListener("scroll", function(){
-    const winScroll = this.window.pageYOffset;
-    const height = this.document.documentElement.scrollHeight - this.window.innerHeight;
-    const scrolled = (winScroll/height) * 100;
-    progressBar.style.width = `${scrolled}%`
-})
-
-
-
-/*----------------------------------------------------------------*/
-/*---------------------------Send Request-------------------------*/
-/*----------------------------------------------------------------*/
-
-
-// function sendRequest() {
-
-//     // Creat a XMLHttpRequest
-//     let xhr = new XMLHttpRequest();
-
-//     // Set API
-//     xhr.open("GET", "URL API", true);
-
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === 4 && xhr.status === 200) {
-//             // Request is ok
-//             let response = JSON.parse(xhr.responseText);
-//             console.log("پاسخ API: " + JSON.stringify(response));
-//         } else if (xhr.readyState === 4) {
-//             // Request is Not ok
-//             console.log("خطا در ریکوئست؛ کد خطا: " + xhr.status);
-//         }
-//     }
-
-//     // Send Request
-//     xhr.send()
-
-// }
-
-
-/*----------------------------------------------------------------*/
-/*---------------+------Send Request Whit AJAX--------------------*/
-/*----------------------------------------------------------------*/
-
-
-// $(document).ready(function () {
-//     $("#getDataBtn").click(function () {
-
-//         // Set API Address
-//         let apiURL = "API ADDRESS";
-
-//         $.ajax({
-//             url: apiURL,
-//             type: "GET",
-//             dataType: "json",
-//             success: function (data) {
-//                 // data Recived
-//                 console.log(data);
-//             },
-//             error: function (xhr, status, error) {
-//                 // ERROR
-//                 console.error("خطا در درخواست " + status, error);
-//             }
-//         })
-//     })
-// })
