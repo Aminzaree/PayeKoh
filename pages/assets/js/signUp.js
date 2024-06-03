e426167a19f5cba92b106ab352a3fdbfa5475076
Amin <124307503+Aminzaree@users.noreply.github.com>
2024-06-03 17:38:02 +0330

    verify for signUp page

--- pages/assets/js/signUp.js
+++ pages/assets/js/signUp.js
@@ -7,10 +7,10 @@ var verifyCodeEmpty = true;
 
 
 function numberValidation() {
-   
-    if(phoneInput.value !== ''){
+
+    if (phoneInput.value !== '') {
         emptyInput = true;
-    } else{
+    } else {
         const Toast = Swal.mixin({
             toast: true,
             position: "top-end",
@@ -18,23 +18,45 @@ function numberValidation() {
             timer: 3000,
             timerProgressBar: true,
             didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
             }
-          });
-          Toast.fire({
+        });
+        Toast.fire({
             icon: "warning",
             title: "لطفا شماره موبایل خود را وارد کنید"
-          });
+        });
         emptyInput = false;
     }
 
-    if(phoneInput !== ''){
-        
+    if (phoneInput.value !== '') {
+
         var numRegex = regex = /^\d*$/;
-        if(phoneInput.value.match(numRegex)){
+        if (phoneInput.value.match(numRegex)) {
             numberValidationRegex = true;
-        } else{
+        } else {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "bottom-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "warning",
+                title: "شماره موبایل فقط می‌تواند عدد باشد."
+            });
+            numberValidationRegex = false
+        }
+
+        let phoneNumber = phoneInput.value;
+
+        if (!phoneInput.value.match(numRegex)) {
+
             const Toast = Swal.mixin({
                 toast: true,
                 position: "top-end",
@@ -42,47 +64,132 @@ function numberValidation() {
                 timer: 3000,
                 timerProgressBar: true,
                 didOpen: (toast) => {
-                  toast.onmouseenter = Swal.stopTimer;
-                  toast.onmouseleave = Swal.resumeTimer;
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
                 }
-              });
-              Toast.fire({
+            });
+            Toast.fire({
                 icon: "warning",
                 title: "شماره موبایل فقط می‌تواند عدد باشد."
-              });
-            numberValidationRegex = false
+            });
+
+            numberCount = false;
+
+        } else if (phoneNumber.length !== 11) {
+
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "warning",
+                title: "شماره موبایل باید 11 رقم باشد"
+            });
+
+            numberCount = false;
+
+        } else if (!phoneNumber.startsWith('09')) {
+
+            numberCount = false;
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "warning",
+                title: "شماره موبایل باید با ٠٩ شروع شود."
+            });
+
+        } else {
+            numberCount = true;
         }
     }
 }
 
+function verifyValidation() {
 
-document.getElementById('sendCode').addEventListener('click', function() {
+    let allFilled = true;
+    for (let input of codeInput) {
+        if (input.value === '') {
+            allFilled = false;
+            break;
+        }
+    }
+    if (!allFilled) {
+        verifyCodeEmpty = false;
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: "warning",
+            title: "لطفا تمامی فیلدها را پر کنید."
+        });
+        emptyInput = false;
+    } else {
+        verifyCodeEmpty = true;
+    }
+
+}
+
+
+document.getElementById('sendCode').addEventListener('click', function () {
 
     numberValidation();
 
-    if(emptyInput == true && numberValidationRegex == true){
+    if (emptyInput == true && numberCount == true) {
         document.getElementById('step1').classList.add('hidden');
-        setTimeout(function() {
+        document.querySelector('.register').classList.add("hidden");
+        document.querySelector('.SignUp-InputBox-other').classList.add("hidden");
+        setTimeout(function () {
             document.getElementById('step1').style.display = 'none';
+            document.querySelector('.register').style.display = 'none';
+            document.querySelector('.SignUp-InputBox-other').style.display = 'none';
             document.getElementById('step2').style.display = 'block';
-            setTimeout(function() {
+            setTimeout(function () {
                 document.getElementById('step2').classList.remove('hidden');
             }, 20);
         }, 500);
     }
 });
 
-document.getElementById('goBack').addEventListener('click', function() {
+document.getElementById('goBack').addEventListener('click', function () {
     var codeInput = document.querySelectorAll(".code-input");
     document.getElementById('step2').classList.add('hidden');
-    setTimeout(function() {
+    setTimeout(function () {
         document.getElementById('step2').style.display = 'none';
         document.getElementById('step1').style.display = 'block';
-        setTimeout(function() {
+        document.querySelector('.register').style.display = 'block';
+        document.querySelector('.SignUp-InputBox-other').style.display = 'flex';
+        setTimeout(function () {
             document.getElementById('step1').classList.remove('hidden');
+            document.querySelector('.register').classList.remove("hidden");
+            document.querySelector('.SignUp-InputBox-other').classList.remove("hidden");
+
         }, 20);
     }, 500);
-    codeInput.forEach(function(input){
+    codeInput.forEach(function (input) {
         input.value = "";
     })
 });
@@ -103,45 +210,11 @@ inputs.forEach((input, index) => {
 });
 
 
-function verifyValidation(){
-    
-    let allFilled = true;
-        for (let input of codeInput) {
-            if (input.value === '') {
-                allFilled = false;
-                break;
-            }
-        }
-        if (!allFilled) {
-            verifyCodeEmpty = false;
-            const Toast = Swal.mixin({
-                toast: true,
-                position: "top-end",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                  toast.onmouseenter = Swal.stopTimer;
-                  toast.onmouseleave = Swal.resumeTimer;
-                }
-              });
-              Toast.fire({
-                icon: "warning",
-                title: "لطفا تمامی فیلدها را پر کنید."
-              });
-            emptyInput = false;
-        } else {
-            verifyCodeEmpty = true;
-        }
+document.getElementById('verifyCode').addEventListener('click', function () {
 
-}
-
-
-document.getElementById('verifyCode').addEventListener('click', function() {
-    
     verifyValidation();
 
-    if(verifyCodeEmpty == true){
+    if (verifyCodeEmpty == true) {
         const code = Array.from(inputs).map(input => input.value).join('');
         alert('کد وارد شده: ' + code);
     }
