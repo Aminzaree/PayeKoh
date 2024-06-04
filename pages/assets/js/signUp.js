c07f1c09e8b7f9b10a2b50020796f9803c52af88
Amin <124307503+Aminzaree@users.noreply.github.com>
2024-06-04 11:31:13 +0330

    verify for signUp page

--- pages/assets/js/signUp.js
+++ pages/assets/js/signUp.js
@@ -1,11 +1,36 @@
-var phoneInput = document.getElementById("phone");
-var codeInput = document.querySelectorAll(".code-input");
-var emptyInput = true;
-var numberValidationRegex = true;
-var numberCount = true;
-var verifyCodeEmpty = true;
+let phoneInput = document.getElementById("phone");
+let sendCodeBtn = document.getElementById("sendCode");
+let verifyCodeBtn = document.getElementById("verifyCode")
+let step1 = document.getElementById("step1");
+let step2 = document.getElementById("step2");
+let codeInput = document.querySelectorAll(".code-input");
+let registerBtn = document.querySelector('.register');
+let otherRegister = document.querySelector('.SignUp-InputBox-other');
+let emptyInput = true;
+let numberValidationRegex = true;
+let numberCount = true;
+let verifyCodeEmpty = true;
 
 
+// Highlight sendCode Button after 11 character 
+phoneInput.addEventListener('input', function() {
+    var phoneNumber = this.value;
+    if (phoneNumber.length === 11) {
+        sendCodeBtn.classList.add('highlight-border');
+    } else {
+        sendCodeBtn.classList.remove('highlight-border');
+    }
+});
+
+
+// Clear Input Value by click on xmark button
+function clearInput(button) {
+    var phoneNumberInput = button.previousElementSibling;
+    phoneNumberInput.value = "";
+}
+
+
+// Number Validation (Step 1)
 function numberValidation() {
 
     if (phoneInput.value !== '') {
@@ -31,7 +56,7 @@ function numberValidation() {
 
     if (phoneInput.value !== '') {
 
-        var numRegex = regex = /^\d*$/;
+        let numRegex = regex = /^\d*$/;
         if (phoneInput.value.match(numRegex)) {
             numberValidationRegex = true;
         } else {
@@ -119,7 +144,9 @@ function numberValidation() {
         }
     }
 }
+  
 
+// Verify code validation (Step 2)
 function verifyValidation() {
 
     let allFilled = true;
@@ -154,18 +181,20 @@ function verifyValidation() {
 }
 
 
-document.getElementById('sendCode').addEventListener('click', function () {
+// Send Code button validation & Request 
+sendCodeBtn.addEventListener('click', function () {
 
     numberValidation();
 
     if (emptyInput == true && numberCount == true) {
+
         document.getElementById('step1').classList.add('hidden');
         document.querySelector('.register').classList.add("hidden");
-        document.querySelector('.SignUp-InputBox-other').classList.add("hidden");
+        otherRegister.classList.add("hidden");
         setTimeout(function () {
             document.getElementById('step1').style.display = 'none';
             document.querySelector('.register').style.display = 'none';
-            document.querySelector('.SignUp-InputBox-other').style.display = 'none';
+            otherRegister.style.display = 'none';
             document.getElementById('step2').style.display = 'block';
             setTimeout(function () {
                 document.getElementById('step2').classList.remove('hidden');
@@ -174,18 +203,73 @@ document.getElementById('sendCode').addEventListener('click', function () {
     }
 });
 
+
+// document.getElementById('sendCodeBtn').addEventListener('click', function () {
+
+//     numberValidation();
+//     let phone = $("#phone").val();
+
+//     if (emptyInput == true && numberCount == true) {
+
+//         // Start Send Axios Request
+//         axios.post('http://213.134.17.109/auth/rest-auth/registration/', {
+//             phone: phone,
+//         }, {
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//             .then(function (response) {
+//                 // در صورتی که پاسخ موفقیت‌آمیز باشد
+//                 step1.classList.add('hidden');
+//                 registerBtn.classList.add("hidden");
+//                 otherRegister.classList.add("hidden");
+
+//                 setTimeout(function () {
+
+//                     step1.style.display = 'none';
+//                     registerBtn.style.display = 'none';
+//                     otherRegister.style.display = 'none';
+//                     step2.style.display = 'block';
+//                     setTimeout(function () {
+//                         step2.classList.remove('hidden');
+//                     }, 20);
+//                 }, 500);
+
+//             })
+//             .catch(function (error) {
+//                 if (error.response) {
+//                     console.log(error.response.data);
+//                     console.log(error.response.status);
+//                     console.log(error.response.headers);
+//                 } else if (error.request) {
+//                     console.log(error.request);
+//                 } else {
+//                     console.log('Error', error.message);
+//                 }
+//                 alert("عدم برقراری ارتباط با سرور");
+//             });
+//             // .catch(function (error) {
+//             //     console.error('There was a problem with the axios operation:', error);
+//             //     // اگر خطایی رخ داد، کاربر در همان مرحله باقی بماند
+//             // });
+//     }
+// });
+
+
+
 document.getElementById('goBack').addEventListener('click', function () {
-    var codeInput = document.querySelectorAll(".code-input");
-    document.getElementById('step2').classList.add('hidden');
+    let codeInput = document.querySelectorAll(".code-input");
+    step2.classList.add('hidden');
     setTimeout(function () {
-        document.getElementById('step2').style.display = 'none';
-        document.getElementById('step1').style.display = 'block';
-        document.querySelector('.register').style.display = 'block';
-        document.querySelector('.SignUp-InputBox-other').style.display = 'flex';
+        step2.style.display = 'none';
+        step1.style.display = 'block';
+        registerBtn.style.display = 'block';
+        otherRegister.style.display = 'flex';
         setTimeout(function () {
-            document.getElementById('step1').classList.remove('hidden');
-            document.querySelector('.register').classList.remove("hidden");
-            document.querySelector('.SignUp-InputBox-other').classList.remove("hidden");
+            step1.classList.remove('hidden');
+            registerBtn.classList.remove("hidden");
+            otherRegister.classList.remove("hidden");
 
         }, 20);
     }, 500);
@@ -193,6 +277,8 @@ document.getElementById('goBack').addEventListener('click', function () {
         input.value = "";
     })
 });
+
+
 
 const inputs = document.querySelectorAll('.code-input');
 inputs.forEach((input, index) => {
@@ -210,7 +296,8 @@ inputs.forEach((input, index) => {
 });
 
 
-document.getElementById('verifyCode').addEventListener('click', function () {
+// Verify Code & register
+verifyCodeBtn.addEventListener('click', function () {
 
     verifyValidation();
 
